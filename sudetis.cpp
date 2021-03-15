625682b8ed1b05a57ff201999f177145ab9dba43
LiLXaMeR <75298537+LiLXaMeR@users.noreply.github.com>
2021-03-15 21:37:58 +0200

    Add files via upload

---
+++ sudetis.cpp
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+
+int
+main ()
+{
+  int x;
+  int y;
+  cout << "Iveskite du skaiciu:" << endl;
+  cin >> x >> y;
+  cout << "Atsakymas: " << x + y << endl;
+  return 0;
+}
