0248e5e374e4db02d73177d11fff163c4ff744c0
LiLXaMeR <75298537+LiLXaMeR@users.noreply.github.com>
2021-04-20 09:08:55 +0300

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string vardas;
+    cout << "Jusu vardas: " << endl;
+    cin >> vardas;
+    cout << "Sveiki " << vardas << endl;
+    return 0;
+}
---
+++ kvadratas.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int
+main ()
+{
+  int x;
+  cout << "Iveskite skaiciu:" << endl;
+  cin>>x;
+  cout << "Jusu atsakymas: " << x*x << endl;
+  return 0;
+}
